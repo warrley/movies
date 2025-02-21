480f399963ae22e8c0e0af1366ac5e1f74ac1d4f
warley farias <157419864+warrley@users.noreply.github.com>
2025-02-21 19:45:55 -0300

    new favorites

--- src/pages/Favorites/index.js
+++ src/pages/Favorites/index.js
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react';
 import './favorites.css'
 import { Link } from 'react-router-dom';
+import { Delete, Trash } from 'lucide-react';
 
 const Favorites = () => {
     const [movie, setMovie] = useState([]);
@@ -12,6 +13,13 @@ const Favorites = () => {
 
     },[])
 
+    const handleDelete = (id) => {
+        const updatedMovies = (movie.filter(movie => movie.id !== id));
+        setMovie(updatedMovies);
+
+        localStorage.setItem("@myList", JSON.stringify(updatedMovies))
+    }
+
     return (
         <div className='my-movies'>
             <h1>My Movies</h1>
@@ -19,10 +27,13 @@ const Favorites = () => {
             <ul>
                 {movie.map(movie => (
                     <li key={movie.id}>
-                        <span>{movie.title}</span>
-                        <div>
-                            <Link to={`/movie/${movie.id}`}>See details</Link>
-                            <button>Delete</button>
+                        <img src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title} />
+                        <div className='hero'>
+                            <span>{movie.title}</span>
+                            <div>
+                                <Link to={`/movie/${movie.id}`}>See details</Link>
+                                <button onClick={() => handleDelete(movie.id)}> <Trash color='blue' fill='currentColor' height="30" width="30"/> </button>
+                            </div>
                         </div>
                     </li>
                 ))}
