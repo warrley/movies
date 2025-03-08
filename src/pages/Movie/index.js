7fc163e177d27f30ed53dfde26bbc4b9eff048c4
warley farias <157419864+warrley@users.noreply.github.com>
2025-03-08 11:14:25 -0300

    details mobile

--- src/pages/Movie/index.js
+++ src/pages/Movie/index.js
@@ -59,22 +59,43 @@ function Movie() {
             <div className="background">
                 <img className="background-image" src={`https://image.tmdb.org/t/p/original/${movie.backdrop_path}`} alt={movie.title} />
             </div>
-            <div className="content">
+            <div className="content desktop">
                 <img className="content-image" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}/>
                 <div className="content-text">
                     <h1>{movie.title}</h1>
 
+                    <h3>Sinopse</h3>
+                    <span>{movie.overview}</span>
+                    <strong>Avaliation: {movie.vote_average} / 10</strong>
+
+                    <div className="buttons-area">
+                        <button onClick={saveMovie}>Save</button>
+                        <button>
+                            <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${movie.title} Trailer`}>Trailler</a>
+                        </button>
+                    </div>
+                </div>
+            </div>
+
+            <div className="mobile">
+                <div className="mob-top">
+                    <img className="img-mob" src={`https://image.tmdb.org/t/p/original/${movie.poster_path}`} alt={movie.title}/>
+                    <div className="mob-text-top">
+                        <div>
+                            <h1>{movie.title}</h1>
+                            <strong>Avaliation: {movie.vote_average} / 10</strong>
+                        </div>
+
+                        <div className="mob-buttons">
+                            <button onClick={saveMovie}>Save</button>
+                            <button>
+                                <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${movie.title} Trailer`}>Trailler</a>
+                            </button>
+                        </div>
+                    </div>
+                </div>
                 <h3>Sinopse</h3>
                 <span>{movie.overview}</span>
-                <strong>Avaliation: {movie.vote_average} / 10</strong>
-
-                <div className="buttons-area">
-                    <button onClick={saveMovie}>Save</button>
-                    <button>
-                        <a target="blank" rel="external" href={`https://youtube.com/results?search_query=${movie.title} Trailer`}>Trailler</a>
-                    </button>
-                </div>
-                </div>
             </div>
         </div>
     )
